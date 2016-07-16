d0470443a0553c164b042a6765766291faed81d2
FishingOnATree <raysung78@gmail.com>
2016-07-16 18:07:01 +0800

    Works but still too slow to be accepted. There are a few places that we can still fine-tune

--- algorithms/373_find_k_pair_with_small_sum.py
+++ algorithms/373_find_k_pair_with_small_sum.py
@@ -1,46 +1,48 @@
 # https://leetcode.com/problems/find-k-pairs-with-smallest-sums/
 # this is indeed a nxm matrix with the next smallest sum next to one of the previously selected coordinate
+import sys
 
 class Solution(object):
 
     def kSmallestPairs(self, nums1, nums2, k):
         return_list = []
-        if nums1 and nums2:
+        if nums1 and nums2 and k > 0:
             # slice nums1 and nums2
-            if len(nums1) > k:
-                nums1 = nums1[0:k]
-            if len(nums2) > k:
-                nums2 = nums2[0:k]
-            possible_combo = len(nums1) * len(nums2)
-            pivot = k if k <= possible_combo else possible_combo
-            tracking_set = set()
+            if len(nums1) > k: nums1 = nums1[0:k]
+            if len(nums2) > k: nums2 = nums2[0:k]
+            return_size = k if k <= len(nums1) * len(nums2) else len(nums1) * len(nums2)
+            tracking_matrix = [[False for _ in nums2] for _ in nums1]
+            edge_col_position = [0] * len(nums1)
+
+            #init first item
             return_list.append([nums1[0], nums2[0]])
-            tracking_set.add(tuple((0, 0)))
-            while len(return_list) < pivot:
-                min_total = nums1[-1] + nums2[-1]
-                min_list = [ [len(nums1)-1, len(nums2)-1]]
-                #TODO need to find edge
-                for u, v in tracking_set:
-                    # 3 steps 1 to right, 1 to down or 1 right and 1 down
-                    if u+1 < len(nums1):
-                        min_total, min_list = self.check_min(tracking_set, min_total, min_list, nums1, nums2, u+1, v)
-                    if v+1 < len(nums2):
-                        min_total, min_list = self.check_min(tracking_set, min_total, min_list, nums1, nums2, u, v+1)
-                    if u+1 < len(nums1) and v+1 < len(nums2):
-                        min_total, min_list = self.check_min(tracking_set, min_total, min_list, nums1, nums2, u+1, v+1)
-                min_list.sort(key=lambda x: x[0])
+            tracking_matrix[0][0] = True
+            edge_col_position[0] = 0
+
+            while len(return_list) < return_size:
+                min_total = sys.maxint
+                min_list = []
+                for row_index, row in enumerate(tracking_matrix):
+                    starting_col = 0 if edge_col_position[row_index] is None else edge_col_position[row_index]
+                    for col_index in range(starting_col, len(row)):
+                        if not row[col_index]:
+                            break
+                    edge_col_position[row_index] = col_index
+                for row_index, col_index in enumerate(edge_col_position):
+                    min_total, min_list = self.check_min(tracking_matrix, min_total, min_list, nums1, nums2, row_index, col_index)
                 for u, v in min_list:
-                    if tuple((u, v)) not in tracking_set:
+                    if not tracking_matrix[u][v]:
                         return_list.append([nums1[u], nums2[v]])
-                        tracking_set.add(tuple((u, v)))
-            return_list = return_list[0:pivot]
+                        tracking_matrix[u][v] = True
+                        edge_col_position[u] = v
+            return_list = return_list[0:return_size]
         return return_list
 
-    def check_min(self, tracking_set, min_sum, min_list, nums1, nums2, u, v):
+    def check_min(self, tracking_matrix, min_sum, min_list, nums1, nums2, u, v):
         method_min = min_sum
-        if tuple((u, v)) not in tracking_set:
+        if not tracking_matrix[u][v]:
             total = nums1[u] + nums2[v]
-            if total < min_sum:
+            if total < method_min:
                 method_min = total
                 min_list = [[u, v]]
             elif total == method_min:
