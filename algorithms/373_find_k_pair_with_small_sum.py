561a779bcbfe4d11fd284cad70ff97942892e447
FishingOnATree <raysung78@gmail.com>
2016-07-16 17:20:16 +0800

    2nd attempt still too slow, needs to examine only the edge of the matrix, not the entire "used" cells.

--- algorithms/373_find_k_pair_with_small_sum.py
+++ algorithms/373_find_k_pair_with_small_sum.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/find-k-pairs-with-smallest-sums/
+# this is indeed a nxm matrix with the next smallest sum next to one of the previously selected coordinate
 
 class Solution(object):
 
@@ -12,14 +13,44 @@ class Solution(object):
                 nums2 = nums2[0:k]
             possible_combo = len(nums1) * len(nums2)
             pivot = k if k <= possible_combo else possible_combo
-            sum_list = [ [[u, n], u+n] for u in nums1 for n in nums2]
-            sum_list.sort(key=lambda x: (x[1], x[0][0]))
-            return_list = [sum_list[index][0] for index in range(pivot)]
+            tracking_set = set()
+            return_list.append([nums1[0], nums2[0]])
+            tracking_set.add(tuple((0, 0)))
+            while len(return_list) < pivot:
+                min_total = nums1[-1] + nums2[-1]
+                min_list = [ [len(nums1)-1, len(nums2)-1]]
+                #TODO need to find edge
+                for u, v in tracking_set:
+                    # 3 steps 1 to right, 1 to down or 1 right and 1 down
+                    if u+1 < len(nums1):
+                        min_total, min_list = self.check_min(tracking_set, min_total, min_list, nums1, nums2, u+1, v)
+                    if v+1 < len(nums2):
+                        min_total, min_list = self.check_min(tracking_set, min_total, min_list, nums1, nums2, u, v+1)
+                    if u+1 < len(nums1) and v+1 < len(nums2):
+                        min_total, min_list = self.check_min(tracking_set, min_total, min_list, nums1, nums2, u+1, v+1)
+                min_list.sort(key=lambda x: x[0])
+                for u, v in min_list:
+                    if tuple((u, v)) not in tracking_set:
+                        return_list.append([nums1[u], nums2[v]])
+                        tracking_set.add(tuple((u, v)))
+            return_list = return_list[0:pivot]
         return return_list
+
+    def check_min(self, tracking_set, min_sum, min_list, nums1, nums2, u, v):
+        method_min = min_sum
+        if tuple((u, v)) not in tracking_set:
+            total = nums1[u] + nums2[v]
+            if total < min_sum:
+                method_min = total
+                min_list = [[u, v]]
+            elif total == method_min:
+                min_list.append([u, v])
+        return method_min, min_list
 
 
 a = Solution()
 print("[1, 7, 11], [2, 4, 6], 3 =>", a.kSmallestPairs([1, 7, 11], [2, 4, 6], 3))
+print("[1, 1, 2], [1, 2, 3], 10 =>", a.kSmallestPairs([1, 1, 2], [1, 2, 3], 10))
 print("[1, 1, 2], [1, 2, 3], 2 =>", a.kSmallestPairs([1, 1, 2], [1, 2, 3], 2))
 print("[1, 2], [3], 3 =>", a.kSmallestPairs([1, 2], [3], 3))
 print("[1, 2, 3, 5, 10], [3, 3, 4, 5, 6], 5 =>", a.kSmallestPairs([1, 2, 3, 5, 10], [3, 3, 4, 5, 6], 5))
