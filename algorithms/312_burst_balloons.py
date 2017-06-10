a0e769b70e4a15d9dac97c518867c92c2c16f7d8
FishingOnATree <raysung78@gmail.com>
2017-06-10 14:14:49 -0400

    LeetCode #312 - answer is correct, but exceeds time limit

--- algorithms/312_burst_balloons.py
+++ algorithms/312_burst_balloons.py
@@ -1,25 +1,36 @@
 __author__ = 'Rays'
+# https://leetcode.com/problems/burst-balloons/#/description
+# DP to try all possible scenarios
 
-
+import random
 
 class Solution(object):
     def maxCoins(self, nums):
-        #greedy algorithm that always remove smallest int -- doesn't work
-        total = 0
-        for _ in range(len(nums)):
-            # find min
-            index = 0
-            for i in range(1, len(nums)):
-                if nums[i] < nums[index]:
-                    index = i
-            left = nums[index-1] if index-1 >= 0 else 1
-            right = nums[index+1] if index+1 < len(nums) else 1
-            total += left * nums[index] * right
-            nums.pop(index)
-        return total
+        self.my_dict = {}
+        return self.findMax(nums)
 
+    def findMax(self, nums):
+        if nums is None or len(nums) == 0:
+            return 0
+        key = tuple(nums)
+        if key not in self.my_dict:
+            max_coins = 0
+            for i in range(len(nums)):
+                new_nums = list(nums)
+                new_nums.pop(i)
+                curr = self.getValue(i - 1, nums) * nums[i] * self.getValue(i+1, nums) + self.findMax(new_nums)
+                #print(i, self.getValue(i - 1, nums), nums[i], self.getValue(i+1, nums), self.getValue(i - 1, nums) * nums[i] * self.getValue(i+1, nums))
+                max_coins = max(max_coins, curr)
+            self.my_dict[key] = max_coins
+            # print(key, max_coins)
+        return self.my_dict[key]
+
+    def getValue(self, i, nums):
+        return nums[i] if 0 <= i <= len(nums) - 1 else 1
 
 a = Solution()
+#print(a.maxCoins([3, 5, 8])) #152
 print(a.maxCoins([3, 1, 5, 8])) #167
-
-print(a.maxCoins([1,2,3,4,5,6,7,8])) #912
+# print(a.maxCoins([1,2,3,4,5,6,7,8])) #912
+for _ in range(10):
+    print([random.randint(0, 100) for _ in range(random.randint(5, 15))])
