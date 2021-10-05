98967476cc188c3e50c7651359c0e3f1075eda4d
Raymond S <raysung78@gmail.com>
2021-10-04 23:41:39 -0400

    Update 2017_grid_game.py
    faster than 5 % but use less memory than 100% --- complexity probably higher than 100%

--- algorithms/2017_grid_game.py
+++ algorithms/2017_grid_game.py
@@ -4,21 +4,40 @@ class Solution:
 
     def gridGame(self, grid: List[List[int]]) -> int:
         n = len(grid[0])
-        # robot A - goal is to minimize the score of Robot B, NOT maximize its own score. 
+        # robot A - goal is to minimize the score of Robot B, NOT maximize its own score.
         # print("ROBOT A ----------------------------------------\n\n ")
         # print("Grid: ", grid)
-        pre_computed = self.precomute_grid(grid, n)
+        pre_computed = self.precomute_robot_a_grid(grid, n)
         # print("pre_comuted: ", pre_computed)
-        points = self.robot_move(grid, n, pre_computed)
+        self.robot_a_move(grid, n, pre_computed)
         # robot B
         # print("ROBOT B ----------------------------------------\n\n ")
         # print("Grid: ", grid)
-        pre_computed = self.precomute_grid(grid, n)
+        pre_computed = self.precomute_robot_b_grid(grid, n)
         # print("pre_comuted: ", pre_computed)
-        points = self.robot_move(grid, n, pre_computed)
+        points = self.robot_b_move(grid, n, pre_computed)
         return points
 
-    def robot_move(self, grid, n, pre_computed):
+    def robot_a_move(self, grid, n, pre_computed):
+        # robot 1
+        pivot = -1
+        max_points = None
+        for i in range(0, n):
+            # print("iteration: ", i, " - ", pre_computed[0][i+1] if i+1 < n else 0 , " - ", pre_computed[1][i-1] if i-1 >= 0 else 0)
+            #max_pt_step = max(pre_computed[0][i+1] if i+1 < n else 0, pre_computed[1][i-1] - pre_computed[1][i] if i-1 >= 0 else pre_computed[1][i])
+            max_pt_step = max(pre_computed[0][i+1] if i+1 < n else 0, pre_computed[1][i-1] if i-1 >= 0 else 0)
+            if max_points == None or max_pt_step < max_points:
+                pivot = i
+                max_points = max_pt_step #new min
+        # print("max_points: ", max_points)
+        # print("pivot: ", pivot)
+        for x in range(0, pivot+1):
+            grid[0][x] = 0
+        for x in range(pivot, n):
+            grid[1][x] = 0
+        # print("Grid after A moved: ", grid)
+
+    def robot_b_move(self, grid, n, pre_computed):
         # robot 1:
         x = 1
         y = n - 1
@@ -41,7 +60,19 @@ class Solution:
         # print("Final grid: ", grid)
         return points
 
-    def precomute_grid(self, grid, n):
+    def precomute_robot_a_grid(self, grid, n):
+        pre_computed = [[0] * n, [0] * n]
+        pre_computed[0][n-1] = grid[0][n-1]
+        pre_computed[1][0] = grid[1][0]
+        for i in range(1, n-1):
+            pre_computed[0][n-i-1] = grid[0][n-i-1] + pre_computed[0][n-i]
+            pre_computed[1][i] = grid[1][i] + pre_computed[1][i-1]
+        if n > 1:
+            pre_computed[1][n-1] = pre_computed[1][n-2]
+        return pre_computed
+
+
+    def precomute_robot_b_grid(self, grid, n):
         pre_computed = [[0] * n, [0] * n]
         pre_computed[0][0] = grid[0][0]
         pre_computed[1][0] = grid[0][0] + grid[1][0]
@@ -52,14 +83,14 @@ class Solution:
 
 
 solution = Solution()
-#solution.gridGame([[2,5,4],[1,5,1]])
+# print(solution.gridGame([[2,5,4],[1,5,1]]))
 print(solution.gridGame([[4, 9, 3], [7, 9, 5]]))
 print(solution.gridGame([[10, 5, 5, 3, 10, 5, 8], [9, 1, 10, 4, 2, 4, 9]]))
 
 import random
 
-MAX = 10
-N_MAX = 5
+MAX = 100
+N_MAX = 10
 
 for i in range(5):
     n = random.randint(1, N_MAX)
